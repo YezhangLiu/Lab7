ad03f73453c132b847506f61aa0ec8c810a79fe2
Aidan Liu <lyz784125253@gmail.com>
2021-05-16 20:35:19 -0700

    done lab

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,41 @@
 // script.js
-
 import { router } from './router.js'; // Router imported so you can use it to manipulate your SPA app here
-const setState = router.setState;
+//const setState = router.setState;
 
 // Make sure you register your service worker here too
-
 document.addEventListener('DOMContentLoaded', () => {
   fetch('https://cse110lab6.herokuapp.com/entries')
-    .then(response => response.json())
-    .then(entries => {
-      entries.forEach(entry => {
-        let newPost = document.createElement('journal-entry');
-        newPost.entry = entry;
-        document.querySelector('main').appendChild(newPost);
+  .then(response => response.json())
+  .then(entries => {
+    entries.forEach((entry, index) => {
+      let newPost = document.createElement('journal-entry');
+      newPost.entry = entry;
+
+      newPost.addEventListener("click", () => {
+        history.pushState({entry, index}, "entry", `#entry${index + 1}`);
+        router.setState("entry", {entry, index});
       });
+      document.querySelector('main').appendChild(newPost);
     });
+  });
+});
+
+// ====
+// init selectors
+// init selectors
+var settingSel = document.querySelector("img");
+var headerSel = document.querySelector("h1");
+
+
+// go to setting 
+settingSel.addEventListener("click", () => {
+  router.setState("settings", null);
+  history.pushState({}, "settings", router.path);
+});
+
+
+// go back to main page
+headerSel.addEventListener("click", () => {
+  router.setState("index", null);
+  history.pushState({}, "index", router.path);
 });
--- sw.js
+++ sw.js
@@ -1,6 +1,92 @@
 // sw.js - Service Worker
-
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 // You will need 3 event listeners:
 //   - One for installation
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    'index.html',
+    'style.css',
+    '/components/entry-page.js',
+    '/components/journal-entry.js',
+    '/scripts/router.js',
+    '/scripts/script.js',
+    'https://cse110lab6.herokuapp.com/entries'
+];
+
+// installation event listener
+self.addEventListener('install', function(event) {
+    // Perform install steps
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+// activation event listener
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache != CACHE_NAME) {
+                        console.log('Clearing old cache');
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    );
+});
+
+// fetch requests event listener
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          return fetch(event.request).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+  
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
+              return response;
+            }
+          );
+        })
+      );
+  });
